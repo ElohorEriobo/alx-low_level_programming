872251a2f90ee4c10d0136bec858ca9f70aa72ce
ElohorEriobo <elohor.ekure@gmail.com>
2022-06-29 14:24:45 -0700

    d

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
- /* main: This is my first C program */
+  /**
+   * This is the preferred style for multi-line
+   * comments in C source code.
+   * Please use it consistently.
+   *
+   * Description:  A column of asterisks on the left side,
+   */
 
 int main(void)
 
