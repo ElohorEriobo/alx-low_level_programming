80d2410236f6f2df350e99d2a0a7ae72ea46fd0f
ElohorEriobo <elohor.ekure@gmail.com>
2022-06-29 15:54:47 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * Description: main - new here                                             
+ * Description: main - new here
  * Good to be here
  * Return: successful
- */                                                                                             
-
+ */
 int main(void)
-{	                                                                    	puts("\"Programming is like building a multilingual puzzle");			return (0);
+{
+	puts("\"Programming is like building a multilingual puzzle");			return (0);
 }
