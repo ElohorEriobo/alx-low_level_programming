2316a948e65d769dc1eaf70a693af59b1e028176
ElohorEriobo <elohor.ekure@gmail.com>
2022-06-30 14:01:51 -0700

    g

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include <stdio.h>                                                                                                          
 
-                                                                                                                            
-
-/**                                                                                                                         
+/**                                                                                                          
  *main - this is the main function
  *Description: This function prints a text
  *Return: 0 to indicate success
@@ -15,7 +13,7 @@ int main(void)
 printf("%s%lu%s\n", "Size of a char: ", sizeof(char), " byte(s)");
 printf("%s%lu%s\n", "Size of an int: ", sizeof(int), " byte(s)");
 printf("%s%lu%s\n", "Size of a long int: ", sizeof(long), " byte(s)");
-printf("%s%lu%s\n", "Size of a long long int: ", sizeof(long long), "byte(s)");
+printf("%s%lu%s\n", "Size of a long long int: ", sizeof(long long), " byte(s)");
 printf("%s%lu%s\n", "Size of a float: ", sizeof(float), " byte(s)");
 return (0);
 
