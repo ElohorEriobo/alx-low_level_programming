57de1c2903f04f63b8a68a35efcdb032bc48a395
ElohorEriobo <elohor.ekure@gmail.com>
2022-07-01 12:28:51 -0700

    o

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,33 +3,16 @@
 
 
 /**
- *
- *  * main - output all single digit number of base 10
- *
- *   *
- *
- *    * Return: 0 if successful
- *
- *     */
+ *main - output all single digit number of base 10
+ * Return: 0 if successful
+ */
 
 int main(void)
 
 {
-
-		int var;
-
-
-
-			for (var = 0; var < 10; var++)
-
-						printf("%d", var);
-
-
-
-				printf("\n");
-
-
-
-					return (0);
-
+int var;
+for (var = 0; var < 10; var++)
+printf("%d", var);
+printf("\n");
+return (0);
 }
