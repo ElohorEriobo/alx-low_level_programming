bd299e95f8dafaacd431c633d38febd07d91cb33
ElohorEriobo <elohor.ekure@gmail.com>
2022-07-01 13:53:10 -0700

    k

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,13 @@ int main(void)
 int var;
 for (var = 0; var < 10; var++)
 {
-putchar((var % 10) + '0');							if (var >= 0 && var < 9)
+putchar((var % 10) + '0');
+if (var >= 0 && var < 9)
 {										putchar(',');
-putchar(' ');																			}
-else										continue;
+putchar(' ');
+}
+else
+continue;
 }
 putchar('\n');
 return (0);
