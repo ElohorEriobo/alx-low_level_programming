dd91cc804e4fa56148599e2863f40a6d1798c710
ElohorEriobo <elohor.ekure@gmail.com>
2022-07-01 09:48:11 -0700

    k

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
 
-
 /**
+ * main - print all letters of the alphabeth in lower case
  *
- *  * main - print all letters of the alphabeth in lower case
+ * Return: 0 if successful
  *
- *   *
- *
- *    * Return: 0 if successful
- *
- *     */
+ */
 
 int main(void)
-
 {
 
-		char var;
-
-
-
-			for (var = 'a'; var <= 'z'; var++)
-
-					{
-
-								putchar(var);
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+char var;
+char var1;
+for (var = 'a'; var <= 'z'; var++)
+{
+	putchar(var);
+}
+for (var1 = 'A'; var1 <= 'Z'; var1++)
+{
+putchar(var1);
+}
+putchar('\n');
+return (0);
 }
