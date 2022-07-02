b98b6cfffc93809c171eb137177b4df10c2421e9
ElohorEriobo <elohor.ekure@gmail.com>
2022-07-01 22:25:46 -0700

    j

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,11 +19,13 @@ for (ii = var + 1; ii < 9; ii++)
 int jj;
 for (jj = ii + 1; jj < 10; jj++)
 {
-putchar((var % 10) + '0');																	putchar((ii % 10) + '0');							putchar((jj % 10) + '0');
-																				if (var != 7)
-																				{
-																				putchar(',');
-																				putchar(' ');
+putchar((var % 10) + '0');
+putchar((ii % 10) + '0');
+putchar((jj % 10) + '0');
+if (var != 7)
+{
+putchar(',');
+putchar(' ');
 }
 else
 continue;
