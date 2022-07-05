fb86872fae0a15f614affcb4a1e69f304f7a4f86
ElohorEriobo <elohor.ekure@gmail.com>
2022-07-05 09:39:21 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ *  main - first
+ *    
+ *
+ *   Return: 0 if success
+ */
+
+int main(void)
+
+{
+int i = 0;
+char text[] = "_putchar";
+while text[i] !='\0'
+{_putchar(text[i]);
+	i++;
+	}
+_putchar('\n');
+		return (0)
+
+}
