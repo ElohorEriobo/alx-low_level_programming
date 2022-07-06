755b8807618fd8ebff72ce5f3f3030b1a8da9d31
ElohorEriobo <elohor.ekure@gmail.com>
2022-07-06 01:10:26 -0700

    5

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -3,6 +3,7 @@
 
 
 /**
+ *
  *  * main - check the code.
  *
  *   *
@@ -14,29 +15,32 @@
 int main(void)
 
 {
-int r;
+
+	    int r;
 
 
-r = print_sign(98);
 
-_putchar(',');
+	        r = print_sign(98);
 
-_putchar(' ');
+		    _putchar(',');
 
-_putchar(r + '0');
+		        _putchar(' ');
 
-_putchar('\n');
+			    _putchar(r + '0');
 
-r = print_sign(0);
+			        _putchar('\n');
 
-_putchar(',');
+				    r = print_sign(0);
 
-_putchar(' ');
+				        _putchar(',');
 
-_putchar(r + '0');
+					    _putchar(' ');
 
-_putchar('\n');
-r = print_sign(0xff);
+					        _putchar(r + '0');
+
+						    _putchar('\n');
+
+						        r = print_sign(0xff);
 
 							    _putchar(',');
 
@@ -46,7 +50,7 @@ r = print_sign(0xff);
 
 								        _putchar('\n');
 
-r = print_sign(-1);
+									    r = print_sign(-1);
 
 									        _putchar(',');
 
